a3e77de31e99e1f86ff7423bb61055df46a1ed5a
dawn-chung27 <dawn.chung27@gmail.com>
2020-12-23 17:14:01 -0800

    create loginpage

---
+++ client/components/LoginPage.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+
+const LoginPage = props => {
+
+  return (
+    
+    <Container className="d-flex height justify-content-center align-items-center">
+      
+      <Form>
+        <h3>Please Sign In</h3>
+        <Form.Group controlId="formUsername">
+          <Form.Control type="text" placeholder="Enter username" />
+        </Form.Group>
+
+        <Form.Group controlId="formPassword">
+          <Form.Control type="password" placeholder="Enter password" />
+        </Form.Group>
+        
+        <Button className="w-100" variant="primary" type="submit">
+          Sign In
+        </Button>
+      </Form>
+      
+    </Container>
+    
+  )
+}
+
+export default LoginPage;
