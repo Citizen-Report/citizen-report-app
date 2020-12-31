7e410c4e046896a30e23cda4da16e011136cef32
Matt Jiang <mattljiang@gmail.com>
2020-12-31 10:17:45 -0800

    remove setReqBody from POST /signup

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -5,6 +5,7 @@ const db = require('../models/model');
 const reportsController = require('../controllers/reportsController');
 const authController = require('../controllers/authController');
 
+// Middleware to set req body for testing purposes
 const setReqBody = (req, res, next) => {
   req.body = {
     email: 'citizen7@gmail.com',
@@ -19,7 +20,6 @@ const setReqBody = (req, res, next) => {
 
 // POST /signup: create user, create session, set ssid cookie, redirect to get homepage
 router.post('/signup', 
-  setReqBody, 
   authController.createUser,
   authController.startSession,
   authController.setSSIDCookie,
@@ -28,7 +28,7 @@ router.post('/signup',
 })
 /**
  * GET /, to get current user's info based on the cookie sent from client. 
- * If valid cookie, send back JSON: { hasSession: true, userInfo: {email, city, access}}
+ * If valid cookie, send back JSON: { hasSession: true, userInfo: {email, city, access} }
  * If no valid cookie, send back JSON: { hasSession: false, userInfo: null}
  * Middleware: getUserInfo, send back as JSON
  */
